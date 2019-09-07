dfd3d3dd4d26fe10c823e27c31f69736072856f0
vaglarov <pasific@abv.bg>
2019-09-07 16:06:35 +0300

    new

---
+++ 02. COMBINATORIAL ALGORITHMS/Lab/VS/COMBINATORIAL ALGORITHMS/01.PermutationsWithoutRepetitionsArray/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Linq;
+
+namespace _01.PermutationsWithoutRepetitionsArray
+{
+    class Program
+    {
+        static char[] elements;
+        static bool[] isUsed;
+        static char[] perputations;
+
+        static void Permute(int index)//index-current cell to fill
+        {
+            if (index >= elements.Length)
+            {
+                PrintPemutetionArray();
+            }
+            else
+            {
+                for (int i = 0; i < elements.Length; i++)
+                {
+                    if (!isUsed[i])
+                    { 
+                        var currentNumberr = elements[i];
+                        isUsed[i] = true;
+                        perputations[index] = currentNumberr;
+                        Permute(index + 1);
+                        isUsed[i] = false;
+                    }
+                }
+            }
+        }
+
+        private static void PrintPemutetionArray()
+        {
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine(string.Join(' ', perputations));
+            Console.ForegroundColor = ConsoleColor.White;
+        }
+
+        static void Main(string[] args)
+        {
+            elements = Console.ReadLine()
+                .Split(' ', StringSplitOptions.RemoveEmptyEntries)
+                .Select(x => char.Parse(x))
+                .ToArray();
+
+               
+            isUsed = new bool[elements.Length];
+            perputations = new char[elements.Length];
+
+            Permute(0);
+        }
+    }
+}
---
+++ 02. COMBINATORIAL ALGORITHMS/Lab/VS/COMBINATORIAL ALGORITHMS/02.PermutationsWithoutRepetitionsSWAP/Program.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Linq;
+
+namespace _02.PermutationsWithoutRepetitions
+{
+    class Program
+    {
+        static char[] elements;
+
+        static int countPermutetion = 0;
+
+        static void Permute(int index)
+        {
+            if (index >= elements.Length)
+            {
+                PrintPemutetionArray();
+            }
+            else
+            {
+                Permute(index + 1);
+                for (int i = index + 1; i < elements.Length; i++)
+                {
+                    Swap(index, i);
+                    Permute(index + 1);
+                    Swap(index, i);
+                }
+            }
+        }
+
+        private static void Swap(int first, int second)
+        {
+            var temp = elements[first];
+            elements[first] = elements[second];
+            elements[second] = temp;
+        }
+
+        private static void PrintPemutetionArray()
+        {
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine(string.Join(' ', elements));
+            countPermutetion++;
+            Console.ForegroundColor = ConsoleColor.White;
+        }
+
+        static void Main(string[] args)
+        {
+            elements = Console.ReadLine()
+                .Split(' ', StringSplitOptions.RemoveEmptyEntries)
+                .Select(x => char.Parse(x))
+                .ToArray();
+            Permute(0);
+            CheckedNumberPermutation();
+        }
+
+        private static void CheckedNumberPermutation()
+        {
+            Console.WriteLine($"Number print permutation: {countPermutetion}");
+            int numberPermutation = Factorial(elements.Length);
+            Console.WriteLine($"Calculate number of permutation: {numberPermutation}");
+            if (numberPermutation == countPermutetion)
+            {
+                Console.WriteLine("Program work well!!");
+            }
+        }
+
+        private static int Factorial(int lastElement)
+        {
+            if (lastElement == 1)
+            {
+                return 1;
+            }
+            else
+            {
+                return lastElement * Factorial(lastElement - 1);
+            }
+        }
+    }
+}
